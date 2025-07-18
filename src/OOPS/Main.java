a067fd6bd6eb77c64c0f29291d2da59510e036cc
rah23ul09 <rahulsinghzwx@gmail.com>
2025-07-18 20:10:14 +0530

    OOPS program added

---
+++ src/OOPS/Main.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+public class Main {
+    public static void main(String[] args) {
+        Encapsulation student = new Encapsulation();
+        student.setName("Rahul");
+        student.setMarks(73);
+
+        System.out.println(student.getName() + " got " + student.getMarks() + " marks");
+    }
+}
