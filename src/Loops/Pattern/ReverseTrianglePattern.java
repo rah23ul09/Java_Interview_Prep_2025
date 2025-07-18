cbe1d630c9bed9dad8151a33c9b6679fbb960dde
rah23ul09 <rahulsinghzwx@gmail.com>
2025-07-18 07:50:38 +0530

    New Changes Updated

--- src/Loops/Pattern/ReverseTrianglePattern.java
+++ src/Loops/Pattern/ReverseTrianglePattern.java
@@ -1,11 +1,11 @@
-package ForLoop.Pattern;
+package Loops.Pattern;
 
-public class PrintSquarePattern {
+public class ReverseTrianglePattern {
     public static void main(String[] args) {
         int row = 5;
 
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= row; j++) {
+        for (int i = row; i >= 1; i--){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
